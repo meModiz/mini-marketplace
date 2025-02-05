f3e57ecc0ff8ec31b462800d53e3351ac9a22697
Modestas A. <modestasanilionis2005@gmail.com>
2025-02-05 22:08:56 +0200

    style(main page, navbar); change style of navbar and main page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,19 +5,22 @@ import ThemeToggle from "@/app/utils/ThemeToggler";
 
 export default function Navbar() {
   return (
-    <nav className="p-4 bg-gray-900 text-white">
-      <ul className="flex gap-4">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/marketplace">Marketplace</Link>
-        </li>
-        <li>
-          <Link href="/about">About us</Link>
-        </li>
-      </ul>
+    <div className="flex flex-row w-full justify-between items-center">
+      <span>COMPANY LOGO</span>
+      <nav className="p-4">
+        <ul className="flex gap-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/marketplace">Marketplace</Link>
+          </li>
+          <li>
+            <Link href="/about">About us</Link>
+          </li>
+        </ul>
+      </nav>
       <ThemeToggle />
-    </nav>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
-  return <div className="bg-red-600 dark:bg-yellow-500">Main Page</div>;
+  return (
+    <div className="flex flex-col w-full max-h-fit bg-lightGradient dark:bg-darkGradient text-black dark:text-white px-80 py-3">
+      <Navbar />
+    </div>
+  );
 }
