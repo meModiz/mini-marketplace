cb5ed7196357875d08ffd339c1ea01f825e31075
Modestas A. <modestasanilionis2005@gmail.com>
2025-02-05 22:08:09 +0200

    chore(project): dependecies small change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,16 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class",
+  darkMode: "selector",
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#fff",
+        backgroundDark: "#181D25",
+      },
+      backgroundImage: {
+        lightGradient: "linear-gradient(to right, #CBC9E9, #E3E8FB, #FFE0F4)",
+        darkGradient: "linear-gradient(to right, #33333B, #2C303E, #392B34)",
       },
     },
   },
