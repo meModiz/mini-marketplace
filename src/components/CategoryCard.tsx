506afcc619c2e3de242f74c79f5e5fd80fdcd5df
Modestas A. <modestasanilionis2005@gmail.com>
2025-02-09 20:31:32 +0200

    style(category_card): correct styling

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -5,14 +5,17 @@ import Image from "next/image";
 export default function CategoryCard({ name, label }: Category) {
   const router = useRouter();
   return (
-    <div
-      className="flex flex-col w-49 h-44.5 bg-transparent transform transition-transform duration-300 hover:scale-105"
-      onClick={() => router.push("/marketplace")} // Make routing with filtration into marketplace
-    >
-      <div className="w-full h-32.5 overflow-hidden rounded-t-xl">
-        <Image alt={label} src={`/${name}.webp`} width={196} height={130} />
+    <div className="flex flex-col overflow-hidden w-49 h-44.5 bg-transparent transform transition-transform duration-300 hover:scale-105">
+      <div className="w-full flex-1 overflow-hidden">
+        <Image
+          alt={label}
+          src={"/" + name + ".webp"}
+          width={196}
+          height={130}
+          className="rounded-t-xl"
+        />
       </div>
-      <div className="text-sm font-medium text-gray-900 bg-white dark:bg-white/10 dark:text-white flex-1 flex items-center justify-center text-center w-full rounded-b-xl">
+      <div className="flex flex-col w-full justify-center items-center bg-white dark:bg-white/5 text-base text-gray-900 dark:text-white rounded-b-xl h-1/3 text-center p-2">
         {label}
       </div>
     </div>
