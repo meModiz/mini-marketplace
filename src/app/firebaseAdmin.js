0ac927d69f2ea6ba1d361c08eadfc4f82622c477
Modestas A. <modestasanilionis2005@gmail.com>
2025-02-18 23:13:45 +0200

    fix(firebase_admin): fix private_key formatting

--- src/app/firebaseAdmin.js
+++ src/app/firebaseAdmin.js
@@ -5,7 +5,7 @@ const serverFirebaseConfig = {
   type: process.env.FIREBASE_SERVER_TYPE,
   project_id: process.env.FIREBASE_SERVER_PROJECT_ID,
   private_key_id: process.env.FIREBASE_SERVER_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_SERVER_PRIVATE_KEY,
+  private_key: process.env.FIREBASE_SERVER_PRIVATE_KEY?.replace(/\\n/g, "\n"),
   client_email: process.env.FIREBASE_SERVER_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_SERVER_CLIENT_ID,
   auth_uri: process.env.FIREBASE_SERVER_AUTH_URI,
